1bd30b2986aff1001b44e084bd34ad6f4ca77522
Amon Benson <amonkbenson@gmail.com>
2022-01-01 18:19:35 +0100

    first working state

--- src/graphlist.js
+++ src/graphlist.js
@@ -1,7 +1,13 @@
+import gates from "./graphs/gates.yml";
+import cnf from "./graphs/cnf.yml";
+import schema from "./graphs/schema.yml";
 import existential from "./graphs/existential.yml";
 import universal from "./graphs/universal.yml";
 
 export default [
+  gates,
+  cnf,
+  schema,
   existential,
   universal
 ]
